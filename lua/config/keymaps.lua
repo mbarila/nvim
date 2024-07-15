30b3485ebf2c5bf608f4897f0aff3c5f9b66b6c6
Matija Barila <barilamatija08@gmail.com>
2024-07-15 08:39:01 +0200

    the checkhealth suggested keymaps not working, commented all variations

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,3 +13,25 @@ wk.register({
   e = { ":NvimTreeToggle<CR>", "Explorer" },
   w = { "VimWiki" }
 }, { prefix = "<leader>" })
+
+-- wk.register(
+-- {
+--     { "<leader>w", group = "VimWiki" },
+--     { "<leader>f", group = "Find" },
+--     { "<leader>e", ":NvimTreeToggle<CR>", desc = "Explorer" },
+--     { "<leader>fb", builtin.buffers, desc = "Buffers" },
+--     { "<leader>ff", builtin.find_files, desc = "Find Files" },
+--     { "<leader>fg", builtin.live_grep, desc = "Live Grep" },
+--     { "<leader>fh", builtin.help_tags, desc = "Help Tags" },
+-- }
+-- )
+
+-- wk.register({
+--   ["<leader>e"] = { ":NvimTreeToggle<CR>", "Explorer" },
+--   ["<leader>f"] = { name = "Find" },
+--   ["<leader>fb"] = { builtin.buffers, "Buffers" },
+--   ["<leader>ff"] = { builtin.find_files, "Find Files" },
+--   ["<leader>fg"] = { builtin.live_grep, "Live Grep" },
+--   ["<leader>fh"] = { builtin.help_tags, "Help Tags" },
+--   ["<leader>w"] = { "", "VimWiki" }
+-- })
