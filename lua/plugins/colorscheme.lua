83b5c838c996a592e08a44ee9c21c39a896da6f3
Matija Barila <barilamatija08@gmail.com>
2024-07-16 09:31:38 +0200

    changed theme from kanagawa to everforest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,31 +1,40 @@
-return {
-    "rebelot/kanagawa.nvim",
+-- return {
+--     "rebelot/kanagawa.nvim",
+--     config = function()
+--       -- Load the Kanagawa theme
+--       require('kanagawa').setup({
+--         -- Configure the theme (optional)
+--         commentStyle = { italic = true },
+--         functionStyle = { bold = true },
+--         keywordStyle = { italic = true },
+--         statementStyle = { bold = true },
+--         typeStyle = { italic = true },
+--         variablebuiltinStyle = { italic = true },
+--         colors = {
+--           palette = {},
+--           theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+--         },
+--         theme = "wave", -- Load "wave" theme by default, options: "wave", "dragon", "lotus"
+--         background = {
+--           dark = "wave", -- Load "wave" theme for dark mode
+--           light = "lotus", -- Load "lotus" theme for light mode
+--         },
+--       })
+--
+--       -- Set the colorscheme
+--       vim.cmd("colorscheme kanagawa")
+--
+--     end
+--   }
+
+return    {
+    'sainnhe/everforest',
+    lazy = false,
+    priority = 1000,
     config = function()
-      -- Load the Kanagawa theme
-      require('kanagawa').setup({
-        -- Configure the theme (optional)
-        commentStyle = { italic = true },
-        functionStyle = { bold = true },
-        keywordStyle = { italic = true },
-        statementStyle = { bold = true },
-        typeStyle = { italic = true },
-        variablebuiltinStyle = { italic = true },
-        colors = {
-          palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-        },
-        overrides = function(colors)
-          return {}
-        end,
-        theme = "wave", -- Load "wave" theme by default, options: "wave", "dragon", "lotus"
-        background = {
-          dark = "wave", -- Load "wave" theme for dark mode
-          light = "lotus", -- Load "lotus" theme for light mode
-        },
-      })
-
-      -- Set the colorscheme
-      vim.cmd("colorscheme kanagawa")
-
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.everforest_enable_italic = true
+        vim.cmd.colorscheme('everforest')
     end
-  }
+}
