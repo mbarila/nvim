8c8e1e26d61b4fbd3511499b122774e0ea726ce6
Matija Barila <barilamatija08@gmail.com>
2024-07-16 08:32:11 +0200

    fixed the which-key keymaps warnings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,36 +2,14 @@ local builtin = require('telescope.builtin')
 local wk = require("which-key")
 
 -- Register which-key descriptions
-wk.register({
-  f = {
-    name = "Find",
-    f = { builtin.find_files, "Find Files" },
-    g = { builtin.live_grep, "Live Grep" },
-    b = { builtin.buffers, "Buffers" },
-    h = { builtin.help_tags, "Help Tags" },
-  },
-  e = { ":NvimTreeToggle<CR>", "Explorer" },
-  w = { "VimWiki" }
-}, { prefix = "<leader>" })
-
--- wk.register(
--- {
---     { "<leader>w", group = "VimWiki" },
---     { "<leader>f", group = "Find" },
---     { "<leader>e", ":NvimTreeToggle<CR>", desc = "Explorer" },
---     { "<leader>fb", builtin.buffers, desc = "Buffers" },
---     { "<leader>ff", builtin.find_files, desc = "Find Files" },
---     { "<leader>fg", builtin.live_grep, desc = "Live Grep" },
---     { "<leader>fh", builtin.help_tags, desc = "Help Tags" },
--- }
--- )
-
--- wk.register({
---   ["<leader>e"] = { ":NvimTreeToggle<CR>", "Explorer" },
---   ["<leader>f"] = { name = "Find" },
---   ["<leader>fb"] = { builtin.buffers, "Buffers" },
---   ["<leader>ff"] = { builtin.find_files, "Find Files" },
---   ["<leader>fg"] = { builtin.live_grep, "Live Grep" },
---   ["<leader>fh"] = { builtin.help_tags, "Help Tags" },
---   ["<leader>w"] = { "", "VimWiki" }
--- })
+wk.add(
+{
+    { "<leader>w", group = "VimWiki" },
+    { "<leader>f", group = "Find" },
+    { "<leader>e", ":NvimTreeToggle<CR>", desc = "Explorer" },
+    { "<leader>fb", function () builtin.buffers() end, desc = "Buffers" },
+    { "<leader>ff", function () builtin.find_files() end, desc = "Find Files" },
+    { "<leader>fg", function () builtin.live_grep() end, desc = "Live Grep" },
+    { "<leader>fh", function () builtin.help_tags() end, desc = "Help Tags" },
+}
+)
