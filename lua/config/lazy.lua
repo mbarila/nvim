8253a559043609ba9db6876fcb0eb8425e136db3
Matija Barila <barilamatija08@gmail.com>
2024-07-16 12:44:58 +0200

    removed unused line

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -55,9 +55,6 @@ require("lazy").setup({
       {'vimwiki/vimwiki'},
 
 },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "kanagawa" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
